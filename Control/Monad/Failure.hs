4301cc13552d88750e2ed6928384b5a858fa50ef
Pepe Iborra <pepeiborra@gmail.com>
2009-10-29 11:30:39 +0100

    Take the MonadLoc stuff out (it will live in its own package now)

--- Control/Monad/Failure.hs
+++ Control/Monad/Failure.hs
@@ -2,16 +2,10 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverlappingInstances #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 {-| Defines the class @MonadFail@ for monads which can fail,
     and the class @MonadLoc@ for monads which support recording
     the source code location and building a stack trace.
-
- * Stack traces are only provided for explicitly annotated program points.
-   For now there is the TH macro 'withLocTH' to help with this.
-   Eventually a preprocessor could be written to automatically insert calls
-   to 'withLoc' at every do statement.
 -}
 module Control.Monad.Failure where
 
@@ -35,54 +29,9 @@ import Control.Monad.RWS
 #endif
 
 import Data.Monoid
-import Language.Haskell.TH.Syntax hiding (lift)
-import Text.PrettyPrint
 
 class Monad m => MonadFail e m where
     failure :: e -> m a
-    failWithSrcLoc :: String -> e -> m a
-    failWithSrcLoc _ = failure
-
--- --------------------------------
--- Error stack traces
--- --------------------------------
-
--- | Generating stack traces for failures
-class MonadLoc a where
-  -- | 'withLoc' records the given source location in the failure trace
-  --   if the underlying monad supports recording stack traces
-  --
-  --   By default 'withLoc' is defined as the identity on its second argument
-  withLoc :: String -> a -> a
-
-instance MonadLoc a where withLoc _ = id
-
-{-| Given a list of source locations and an error, @showFailWithStackTrace@ produces output of the form
-
->       <exception details>
->        in <module a>(<file a.hs>): (12,6)
->           <module b>(<file b.hs>): (11,7)
->           ...
-
--}
-showFailWithStackTrace :: Show e => [String] -> e -> String
-showFailWithStackTrace trace e = render$
-             text (show e) $$
-             text " in" <+> (vcat (map text $ reverse trace))
-
--- | 'withLocTH' is a convenient TH macro which expands to 'withLoc' @\<source location\>@
---   Usage:
---
---  > f x = $withLocTH $ do
-withLocTH :: Q Exp
-withLocTH = do
-  loc <- qLocation
-  let loc_msg = showLoc loc
-  [| withLoc loc_msg |]
- where
-   showLoc Loc{loc_module=mod, loc_filename=filename, loc_start=start} = render $
-                     {- text package <> char '.' <> -}
-                     text mod <> parens (text filename) <> colon <+> text (show start)
 
 -- ----------
 -- Instances
