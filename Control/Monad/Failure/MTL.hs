fbe415cb6042ab4f6d01b90c42ac8ed0c2cd40d6
José Iborra <pepe@monad.local>
2009-12-06 21:08:44 +0100

    Actually, the Applicative constraints can be removed by replacing MonadFailure constraints with separate Monad and Failure constraints.

--- Control/Monad/Failure/MTL.hs
+++ Control/Monad/Failure/MTL.hs
@@ -7,7 +7,6 @@ module Control.Monad.Failure.MTL (module Control.Failure) where
 
 import Control.Failure hiding (Error)
 
-import Control.Applicative (Applicative)
 import Control.Monad.Error
 import Control.Monad.List
 import Control.Monad.Reader
@@ -26,53 +25,53 @@ import Data.Monoid
 instance (Error e, Monad m) => Failure e (ErrorT e m) where
   failure = throwError
 
-instance MonadFailure e m => Failure e (ListT m) where
+instance (Monad m, Failure e m) => Failure e (ListT m) where
   failure = lift . failure
 
-instance MonadFailure e m => Failure e (ReaderT r m) where
+instance (Monad m, Failure e m) => Failure e (ReaderT r m) where
   failure = lift . failure
 
-instance (Monoid w, MonadFailure e m) => Failure e (Lazy.WriterT w  m) where
+instance (Monoid w, Monad m, Failure e m) => Failure e (Lazy.WriterT w  m) where
   failure = lift . failure
 
-instance MonadFailure e m => Failure e (Lazy.StateT s m) where
+instance (Monad m, Failure e m) => Failure e (Lazy.StateT s m) where
   failure = lift . failure
 
-instance (Monoid w, MonadFailure e m) => Failure e (Lazy.RWST r w s m) where
+instance (Monoid w, Monad m, Failure e m) => Failure e (Lazy.RWST r w s m) where
   failure = lift . failure
 
-instance (Monoid w, MonadFailure e m) => Failure e (Strict.WriterT w  m) where
+instance (Monoid w, Monad m, Failure e m) => Failure e (Strict.WriterT w  m) where
   failure = lift . failure
 
-instance MonadFailure e m => Failure e (Strict.StateT s m) where
+instance (Monad m, Failure e m) => Failure e (Strict.StateT s m) where
   failure = lift . failure
 
-instance (Monoid w, MonadFailure e m) => Failure e (Strict.RWST r w s m) where
+instance (Monoid w, Monad m, Failure e m) => Failure e (Strict.RWST r w s m) where
   failure = lift . failure
 
 -- ---------------------
 -- WrapFailure instances
 -- ---------------------
 
-instance (WrapFailure e m, Applicative m, Monad m) => WrapFailure e (ListT m) where
+instance (WrapFailure e m, Monad m) => WrapFailure e (ListT m) where
   wrapFailure f = ListT . wrapFailure f . runListT
 
-instance (WrapFailure e m, Applicative m, Monad m) => WrapFailure e (ReaderT r m) where
+instance (WrapFailure e m, Monad m) => WrapFailure e (ReaderT r m) where
   wrapFailure f m = ReaderT $ \r -> wrapFailure f (runReaderT m r)
 
-instance (WrapFailure e m, Monoid w, Applicative m, Monad m) => WrapFailure e (Lazy.WriterT w m) where
+instance (WrapFailure e m, Monoid w, Monad m) => WrapFailure e (Lazy.WriterT w m) where
   wrapFailure f = Lazy.WriterT . wrapFailure f . Lazy.runWriterT
 
 
 -- all the following instances require undecidable instances
-instance (WrapFailure e m, Applicative m, Monad m) => WrapFailure e (Lazy.StateT s m) where
+instance (WrapFailure e m, Monad m) => WrapFailure e (Lazy.StateT s m) where
   wrapFailure f m = Lazy.StateT $ \s -> wrapFailure f (Lazy.runStateT m s)
 
-instance (WrapFailure e m, Monoid w, Applicative m, Monad m) => WrapFailure e (Lazy.RWST r w s m) where
+instance (WrapFailure e m, Monoid w, Monad m) => WrapFailure e (Lazy.RWST r w s m) where
   wrapFailure f m = Lazy.RWST $ \r s -> wrapFailure f (Lazy.runRWST m r s)
 
-instance (WrapFailure e m, Applicative m, Monad m) => WrapFailure e (Strict.StateT s m) where
+instance (WrapFailure e m, Monad m) => WrapFailure e (Strict.StateT s m) where
   wrapFailure f m = Strict.StateT $ \s -> wrapFailure f (Strict.runStateT m s)
 
-instance (WrapFailure e m, Monoid w, Applicative m, Monad m) => WrapFailure e (Strict.RWST r w s m) where
+instance (WrapFailure e m, Monoid w, Monad m) => WrapFailure e (Strict.RWST r w s m) where
   wrapFailure f m = Strict.RWST $ \r s -> wrapFailure f (Strict.runRWST m r s)
